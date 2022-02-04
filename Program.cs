db124f617f4f92cbe93dab68bd5670a9b96fb8d8
Студент ТУЗ <Student@MyKMK.kulmk.ru>
2022-02-04 11:24:38 +0300

    добавила коммит

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RusaevaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 81;
+            Console.WriteLine($"Вывод: {x}");
+        }
+    }
+}
